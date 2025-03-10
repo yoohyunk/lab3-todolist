4047dafbd62e83d1a88b9a1b8c91c8b216037d8e
yoohyunk <yoohyunk20@gmail.com>
2025-03-10 01:47:12 -0600

    Use the useState hook to define a state variable to store the list of tasks.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [tasks, setTasks] = useState<string[]>([
+    "Do laundry",
+    "Go to gym",
+    "Walk dog",
+  ]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
